a45b1e7fc0cb650602808d1d288dbcb47dfa94e9
Luis Llamas <luisllamas@hotmail.com>
2018-11-06 09:59:17 +0100

    Add files via upload

---
+++ src/GammaCorrectionLib.h
@@ -0,0 +1,21 @@
+#ifndef _GammaCorrectionLib_h
+#define _GammaCorrectionLib_h
+
+#if defined(ARDUINO) && ARDUINO >= 100
+#include "arduino.h"
+#else
+#include "WProgram.h"
+#endif
+
+class GammaCorrection
+{
+	public:
+	
+		static uint8_t Correct8(uint8_t value);
+		static uint8_t CorrectTable8(uint8_t value);
+
+	private:
+		const static uint8_t * PROGMEM Table8;
+};
+
+#endif
